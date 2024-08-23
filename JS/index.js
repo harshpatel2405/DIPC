1d247613be5191557d9f95344727f8180ab3d04f
harshpatel2405 <harshpatel4504@gmail.com>
2024-08-23 17:10:46 +0530

    Day 1 Complete Update (22/08/2024)

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,23 @@
 // Day 1 (22/08/2024)  : Variables and Keywords 
 
+// var , let and const
+
+//var can be called again and again as it is 
+console.log("var keyword");
+var x=55;
+console.log("First x = ",x);
+
+var x=44;
+console.log("Second x = ",x);
+
+console.log("let Keyword");
+let y=78;
+console.log("First y = ",y);
+
+// let y=44;
+// console.log(y); // here y will not print as it is alrady been declared and used.
+
+console.log("Const Keyword");
+const pi=3.14;
+// let pi=3.14;  Here pi cannot be declared again and cannot be updated as its value is now fixed....
+console.log("First pi = ",pi)
